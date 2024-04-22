fd772ec30b2d63f122b6818f759d6aa346a7354d
ShubhojeetSarkar <58595656+ShubhojeetSarkar@users.noreply.github.com>
2024-04-22 10:25:10 -0700

    Update UserController.java

--- Backend/src/main/java/com/backend/fullstackbackend/controller/UserController.java
+++ Backend/src/main/java/com/backend/fullstackbackend/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/* Created by Arjun Gautam */
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
